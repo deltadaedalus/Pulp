ff7a474c7aae26e8ebc073c23978e3bbc548eb7b
Chris Renden <renden.christopher@gmail.com>
2017-11-15 22:11:50 -0700

    A lot, I don't really remember what

--- Pulp/Assets/Fighter.cs
+++ Pulp/Assets/Fighter.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fighter : MonoBehaviour {
+    public int playerNum;
+    public SpriteRenderer hurtSprite;
+    public SpriteRenderer hitSprite;
+    public SpriteRenderer blockSprite;
+
     Animator animator;
     SpriteRenderer sprite;
+    bool canJump = true;
+    int prevStateHash;
+    int stateHash;
+    InputBuffer input;
 
     int dir;
 
@@ -12,16 +21,92 @@ public class Fighter : MonoBehaviour {
 	void Start () {
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        hurtSprite = transform.Find("Hurt").gameObject.GetComponent<SpriteRenderer>();
+        hitSprite = transform.Find("Hit").gameObject.GetComponent<SpriteRenderer>();
+        blockSprite = transform.Find("Block").gameObject.GetComponent<SpriteRenderer>();
         dir = 1;
-	}
+        canJump = true;
+
+        input = new InputBuffer(playerNum.ToString());
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        prevStateHash = stateHash;
+        stateHash = animator.GetCurrentAnimatorStateInfo(0).nameHash;
+
+        if (stateHash != prevStateHash)
+        {
+            StateChange();
+        }
+
+        input.Check();
         FeedMachine();
+        MoveBehaviour();
+        UpdateColliders();
     }
 
     void FeedMachine()
     {
+        //send inputs
+        animator.SetBool("Block", input.block.IsActive());
+        animator.SetBool("Down", input.down.IsActive());
+
+        if (input.attack.CountPress() > 0 && !animator.GetBool("Attack"))
+        {
+            input.attack.ConsumePress();
+            animator.SetTrigger("Attack");
+        }
+
+        while (input.jump.CountPress() > 0 && !canJump)
+            input.jump.ConsumePress();
+        if (input.jump.CountPress() > 0 && !animator.GetBool("Jump"))
+        {
+            input.jump.ConsumePress();
+            animator.SetTrigger("Jump");
+        }
+
+        if (input.special.CountPress() > 0 && !animator.GetBool("Special"))
+        {
+            input.special.ConsumePress();
+            animator.SetTrigger("Special");
+        }
+        
+
+        if (dir == 1)
+        {
+            if (input.right.CountPress() > 0)
+            {
+                input.right.ConsumePress();
+                animator.SetTrigger("Forward");
+            }
+            if (input.left.CountPress() > 0)
+            {
+                input.left.ConsumePress();
+                animator.SetTrigger("Back");
+                Flip();
+            }
+        } else
+        {
+            if (input.left.CountPress() > 0)
+            {
+                input.left.ConsumePress();
+                animator.SetTrigger("Forward");
+            }
+            if (input.right.CountPress() > 0)
+            {
+                input.right.ConsumePress();
+                animator.SetTrigger("Back");
+                Flip();
+            }
+        }
+
+
+        //expire inputs
+        //if (dir == 1 && )
+
+
+        /*Old
         if (Input.GetAxis("Horizontal") != 0)
             if ((Input.GetAxis("Horizontal") > 0) == (dir == -1))
             {
@@ -57,11 +142,166 @@ public class Fighter : MonoBehaviour {
         }
         if (Input.GetKeyDown("space"))
             animator.SetTrigger("Jump");
+            */
+    }
+
+    void MoveBehaviour()
+    {
+
+    }
+
+    void UpdateColliders()
+    {
+
+    }
+
+    void StateChange()
+    {
+
     }
 
     void Flip()
     {
         dir = -dir;
         sprite.flipX = (dir == -1);
+        hitSprite.flipX = (dir == -1);
+        hurtSprite.flipX = (dir == -1);
+        blockSprite.flipX = (dir == -1);
+    }
+}
+
+class InputBuffer
+{
+    public TrackedInput up;
+    public TrackedInput down;
+    public TrackedInput left;
+    public TrackedInput right;
+    public TrackedInput attack;
+    public TrackedInput block;
+    public TrackedInput jump;
+    public TrackedInput special;
+
+    public InputBuffer(string playerTag)
+    {
+        attack = new TrackedInput("Attack" + playerTag, 0.5f, false);
+        block = new TrackedInput("Block" + playerTag, 0.5f, false);
+        jump = new TrackedInput("Jump" + playerTag, 0.5f, false);
+        special = new TrackedInput("Special" + playerTag, 0.5f, false);
+        right = new TrackedInput("Horizontal" + playerTag, 0.01f, false);
+        left = new TrackedInput("Horizontal" + playerTag, -0.01f, true);
+        up = new TrackedInput("Vertical" + playerTag, 0.01f, false);
+        down = new TrackedInput("Vertical" + playerTag, -0.01f, true);
+    }
+
+    public void Check()
+    {
+        attack.Check();
+        block.Check();
+        jump.Check();
+        special.Check();
+        up.Check();
+        down.Check();
+        left.Check();
+        right.Check();
+    }
+}
+
+class TrackedInput
+{
+    float buffer = 0.5f;    //Amount of time in seconds during which an input exists before auto-consume
+
+    string axis;    //Input axis this input is tied to
+    float cutoff;   //Threshold above which this input is considered active
+    bool inverted;  //If true, input will be considered active below threshold
+    
+    float value;    //Current value of the axis
+    bool active;    //Whether the input is currently considered active
+
+    float lastPress;    //Time of last press
+    float lastRelease;  //Time of last release
+
+    Queue<float> presses;   //Queue of buffered inputs
+    Queue<float> releases;  //Queue of buffered releases
+
+    public TrackedInput(string axis, float cutoff, bool inverted)
+    {
+        this.axis = axis;
+        this.cutoff = cutoff;
+        this.inverted = inverted;
+
+        presses = new Queue<float>();
+        releases = new Queue<float>();
+    }
+
+    public void setBuffer(float b)
+    {
+        buffer = b;
+    }
+
+    public float getBuffer()
+    {
+        return buffer;
+    }
+
+    public void Check()
+    {
+        value = Input.GetAxis(axis);
+        if (!inverted && value > cutoff || inverted && value < cutoff)
+        {
+            if (!active)
+            {
+                active = true;
+                presses.Enqueue(Time.time);
+            }
+        } else
+        {
+            if (active)
+            {
+                active = false;
+                releases.Enqueue(Time.time);
+            }
+        }
+
+        while (presses.Count > 0 && presses.Peek() < Time.time - buffer)
+        {
+            presses.Dequeue();
+        }
+    }
+
+    public int CountPress()
+    {
+        return presses.Count;
+    }
+    public int CountRelease()
+    {
+        return releases.Count;
+    }
+
+    public float PeekPress()
+    {
+        return presses.Peek();
+    }
+    public float PeekRelease()
+    {
+        return releases.Peek();
+    }
+
+    public float ConsumePress()
+    {
+        return presses.Dequeue();
+    }
+    public float ConsumeRelease()
+    {
+        return releases.Dequeue();
+    }
+
+    public bool IsActive()
+    {
+        return active;
+    }
+
+    public float GetValue()
+    {
+        return value;
     }
 }
---
+++ Pulp/Assets/FighterManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct FighterColState
+{
+
+}
+
+public class FighterManager : MonoBehaviour {
+    public Fighter[] fighters;
+    public FighterColState[,] colStates;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DetectCollisions();
+	}
+
+    void AddFighter()
+    {
+
+    }
+
+    int t = 0;
+
+    void DetectCollisions()
+    {
+        for (int a = 0; a < fighters.Length; ++a)
+        {
+            SpriteRenderer ahurt = fighters[a].hurtSprite;
+            SpriteRenderer ahit = fighters[a].hitSprite;
+            Vector2 aPos = new Vector2(fighters[a].transform.position.x, fighters[a].transform.position.y);
+
+            for (int b = a+1; b < fighters.Length; ++b)
+            {
+                SpriteRenderer bhurt = fighters[b].hurtSprite;
+                SpriteRenderer bhit = fighters[b].hitSprite;
+                Vector2 bPos = new Vector2(fighters[b].transform.position.x, fighters[b].transform.position.y);
+
+                if (ahurt.gameObject.activeInHierarchy && bhit.gameObject.activeInHierarchy
+                    && polyCollision(translate(ahurt.sprite.vertices, aPos), translate(bhit.sprite.vertices, bPos)))
+                    Debug.Log("A hurts B");
+                if (bhurt.gameObject.activeInHierarchy && ahit.gameObject.activeInHierarchy
+                    && polyCollision(translate(bhurt.sprite.vertices, bPos), translate(ahit.sprite.vertices, aPos)))
+                    Debug.Log("B hurts A");
+            }
+        }
+    }
+
+    Vector2[] translate(Vector2[] verts, Vector2 t)
+    {
+        Vector2[] nv = new Vector2[verts.Length];
+        for (int i = 0; i < verts.Length; ++i)
+        {
+            nv[i] = verts[i] + t;
+        }
+        return nv;
+    }
+
+    bool polyCollision(Vector2[] I, Vector2[] J)
+    {
+        //Check if any edges cross
+        for (int i = 0; i < I.Length; ++i)
+        {
+            int ix = (i + 1) % I.Length;
+            float ai = I[ix].y - I[i].y;
+            float bi = I[i].x = I[ix].x;
+            float ci = ai * I[i].x + bi * I[i].y;
+
+            for (int j = 0; j < J.Length; ++j)
+            {
+                int jx = (j + 1) % J.Length;
+                float aj = J[jx].y - J[j].y;
+                float bj = J[j].x = J[jx].x;
+                float cj = aj * J[j].x + bj * J[j].y;
+
+                float det = ai * bj - aj * bi;
+                float x = (bj * ci - bi * cj) / det;
+                float y = (ai * cj - aj * ci) / det;
+
+                if ((x > I[i].x) == (x < I[ix].x))
+                {
+                    //Debug.Log(x + " " + y);
+                    Debug.DrawLine(new Vector3(I[i].x, I[i].y, 0), new Vector3(I[ix].x, I[ix].y), Color.cyan, 1f);
+                    Debug.DrawLine(new Vector3(J[j].x, J[j].y, 0), new Vector3(J[jx].x, J[jx].y), Color.yellow, 1f);
+                    return true;
+                }
+            }
+        }
+
+        //check if J0 inside I
+        Vector2 v = J[0];
+        int count = 0;
+        for (int i = 0; i < I.Length; ++i)
+        {
+            int ix = (i + 1) % I.Length;
+            if (  ((I[i].y > v.y) == (I[ix].y < v.y))  &&  (v.y < Mathf.Lerp(I[i].x, I[ix].x, (v.y - I[i].y) / (I[ix].y - I[i].y)))  )
+                ++count;
+        }
+
+        if (count % 2 == 1)
+        {
+            //Debug.Log(v.x + " " + v.y);
+            Debug.DrawLine(new Vector3(v.x, v.y, 0), new Vector3(v.x, v.y-0.1f, 0), Color.cyan, 1f);
+            return true;
+        }
+
+        //check if I0 inside J
+        v = I[0];
+        count = 0;
+        for (int i = 0; i < J.Length; ++i)
+        {
+            int ix = (i + 1) % J.Length;
+            if (((J[i].y > v.y) == (J[ix].y < v.y)) && (v.y < Mathf.Lerp(J[i].x, J[ix].x, (v.y - J[i].y) / (J[ix].y - J[i].y))))
+                ++count;
+        }
+
+        if (count % 2 == 1)
+        {
+            Debug.Log(v.x + " " + v.y);
+            return true;
+        }
+
+        //All cases fail
+        return false;
+    }
+}
